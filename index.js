ff197a9753dd1179eb8f1742878945453767a678
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2022-04-08 15:36:29 +0530

    done with 100%

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 showtoast();
- function showtoast () {
-    let toastElList = [].slice.call(document.querySelectorAll('.toast'))
-    let toastList = toastElList.map(function(toastEl) {
-      return new bootstrap.Toast(toastEl)
-    })
-    toastList.forEach(toast => toast.show())
+function showtoast() {
+  let toastElList = [].slice.call(document.querySelectorAll(".toast"));
+  let toastList = toastElList.map(function (toastEl) {
+    return new bootstrap.Toast(toastEl);
+  });
+  toastList.forEach((toast) => toast.show());
 }
 
 let time = new Date().getHours();
 let msg = document.getElementById("greeting");
-if(time >= 0 && time < 12) {
-    msg.textContent = "Good Morning, Guest!"
-}
-else if(time > 12 && time < 18) {
-    msg.textContent = "Good Afternoon, Guest!";
+if (time >= 0 && time < 12) {
+  msg.textContent = "Good Morning, Guest!";
+} else if (time > 12 && time < 18) {
+  msg.textContent = "Good Afternoon, Guest!";
 } else {
-    msg.textContent = "Good Evening, Guest!";
+  msg.textContent = "Good Evening, Guest!";
+}
+let id;
+function debounce(func, delay) {
+  clearTimeout(id);
+
+  id = setTimeout(function () {
+    func();
+  }, delay);
+}
+function changeColor() {
+  let input = document.getElementById("input").value;
+  if (input.length === 0) {
+    document.body.style.backgroundColor = "white";
+  }
+  document.body.style.backgroundColor = `${input}`;
 }
